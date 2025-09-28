3bfa612f311ed84a3d04b9c9fa76c00955366bf4
clamlum <clamlum2@gmail.com>
2025-09-28 18:40:20 +1300

    added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+REPO_URL_HTTPS="https://github.com/clamlum2/nix-config.git"
+REPO_URL_SSH="git@github.com:clamlum2/nix-config.git"
+CURRENT_URL=$(git config --get remote.origin.url 2>/dev/null)
+
+if [ "$CURRENT_URL" = "$REPO_URL_HTTPS" ] || [ "$CURRENT_URL" = "$REPO_URL_SSH" ]; then
+    sudo rsync -av --exclude='.git' --exclude='README.md' "$CONFIG_DIR/" /etc/nixos/
+    sudo nixos-rebuild switch --generation-name "config updated"
+    reboot
+else
+    cd ~
+    git clone "$REPO_URL_HTTPS"
+    cd nix-config
+    sudo rsync -av --exclude='.git' --exclude='README.md' "$CONFIG_DIR/" /etc/nixos/
+    sudo nixos-rebuild switch --generation-name "config installed"
+    reboot
+fi
