1d3d2db7c3c075f5b5df31f7b697a68d370061eb
clamlum <clamlum2@gmail.com>
2025-09-29 13:04:46 +1300

    added hyprshade re-enabling

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ if [ -d "$CONFIG_DIR/.git" ]; then
     git pull origin "$CURRENT_BRANCH"
     sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' "$CONFIG_DIR/" /etc/nixos/
     sudo nixos-rebuild switch --profile-name "config updated ($CURRENT_BRANCH)"
+    hyprshade on extravibrance
 else
     echo "Repo not found. Cloning branch '$BRANCH' to $CONFIG_DIR..."
     git clone --branch "$BRANCH" "$REPO_URL" "$CONFIG_DIR"
     cd "$CONFIG_DIR" || { echo "Failed to cd to $CONFIG_DIR"; exit 1; }
     sudo rsync -av --exclude='.git' --exclude='README.md' --exclude='install.sh' "$CONFIG_DIR/" /etc/nixos/
     sudo nixos-rebuild switch --profile-name "config installed ($BRANCH)"
+    hyprshade on extravibrance
 fi
