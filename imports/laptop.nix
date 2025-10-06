f625ffdf6aa3d2830bdc992e1daa6a1985e3f9ae
clamt <clamlum2@gmail.com>
2025-10-06 13:49:05 +1300

    fixed laptop config

--- imports/laptop.nix
+++ imports/laptop.nix
@@ -4,7 +4,7 @@
   networking.hostName = "laptop";
 
   environment.systemPackages = with pkgs; [
-    pkgs.linuxKernel.packages.linux_zen.broadcom_sta
+    pkgs.linuxKernel.packages.linux_6_16.broadcom_sta
     pkgs.upower
   ];
 
@@ -13,7 +13,6 @@
   boot.blacklistedKernelModules = [ "b43" "ssb" "bcma" "brcm80211" "brcmfmac" "brcmsmac" "bcmdhd" ];
   nixpkgs.config.permittedInsecurePackages = [
     "broadcom-sta-6.30.223.271-57-6.16.7"
-    "broadcom-sta-6.30.223.271-57-6.16.5"
   ];
 
   programs.nix-ld.enable = true;
