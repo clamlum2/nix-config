911705b7d1a19544a047f529c2cc275c624dd182
clamt <clamlum2@gmail.com>
2025-10-06 13:56:40 +1300

    added exception for wifi driver

--- imports/laptop.nix
+++ imports/laptop.nix
@@ -13,6 +13,7 @@
   boot.blacklistedKernelModules = [ "b43" "ssb" "bcma" "brcm80211" "brcmfmac" "brcmsmac" "bcmdhd" ];
   nixpkgs.config.permittedInsecurePackages = [
     "broadcom-sta-6.30.223.271-57-6.16.7"
+    "broadcom-sta-6.30.223.271-57-6.16.9"
   ];
 
   programs.nix-ld.enable = true;
