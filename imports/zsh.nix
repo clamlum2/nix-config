90d87a942a6a8105aff20e3de28c5a6def342257
clamlum <clamlum2@gmail.com>
2025-10-08 00:00:52 +1300

    theme changes

--- imports/zsh.nix
+++ imports/zsh.nix
@@ -58,10 +58,11 @@
     PROMPT+="%{$fg[cyan]%}%~%  "
     PROMPT+="%(?:%{$fg[green]%}%1{➜%} :%{$fg[red]%}%1{➜%} )%{$reset_color%}"
 
+    RPROMPT='$(git_prompt_info)'
+
     ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[cyan]%}git:(%{$fg[blue]%}"
-    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} $(git_prompt_ahead) "
+    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
     ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[cyan]%}) %{$fg[yellow]%}%1{✗%}"
     ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[cyan]%})"
-    RPROMPT='$(git_prompt_info)$(git_prompt_ahead)'
   '';
 }
