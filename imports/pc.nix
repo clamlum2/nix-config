bf6cb08691390cc97cb882180e5cc6ad9a54645a
clamt <clamlum2@gmail.com>
2025-10-08 15:05:20 +1300

    tried changing nvidia driver to latest

--- imports/pc.nix
+++ imports/pc.nix
@@ -42,7 +42,7 @@
 
     # Modesetting is required.
     modesetting.enable = true;
-    
+
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
@@ -59,13 +59,13 @@
     # supported GPUs is at: 
     # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
 	  # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 }
