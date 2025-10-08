5ecaa9e707166b1c4bb0e90d083e6398743d136e
clamt <clamlum2@gmail.com>
2025-10-08 15:55:33 +1300

    moved back to stable kernel

--- configuration.nix
+++ configuration.nix
@@ -34,7 +34,7 @@ in
   #boot.loader.grub.useOSProber = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_stable;
+  boot.kernelPackages = pkgs.linuxPackages;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
