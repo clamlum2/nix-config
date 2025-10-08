d689ddb7a547267d57c9a2e2b1979ea1f6fad611
clamt <clamlum2@gmail.com>
2025-10-08 14:53:48 +1300

    fixed theme

--- imports/zsh.nix
+++ imports/zsh.nix
@@ -52,5 +52,17 @@
     # fi
   '';
 
+  home.file.".oh-my-zsh/custom/themes/custom.zsh-theme".text = ''
+    PROMPT="%F{cyan}%n@%f"
+    PROMPT+="%{$fg[blue]%}%M "
+    PROMPT+="%{$fg[cyan]%}%~%  "
+    PROMPT+="%(?:%{$fg[green]%}%1{➜%} :%{$fg[red]%}%1{➜%} )%{$reset_color%}"
 
+    RPROMPT='$(git_prompt_info)'
+
+    ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[cyan]%}git:(%{$fg[blue]%}"
+    ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+    ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[cyan]%}) %{$fg[yellow]%}%1{✗%}"
+    ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[cyan]%})"
+  '';
 }
