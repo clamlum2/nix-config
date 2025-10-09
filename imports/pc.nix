51080ca2372a604af3929b5b834a92744e9d4a7e
clamt <clamlum2@gmail.com>
2025-10-09 15:17:54 +1300

    updated sunshine config

--- imports/pc.nix
+++ imports/pc.nix
@@ -73,6 +73,12 @@
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
-
   };
+
+  security.wrappers.sunshine = {
+        owner = "root";
+        group = "root";
+        capabilities = "cap_sys_admin+p";
+        source = "${pkgs.sunshine}/bin/sunshine";
+ };
 }
