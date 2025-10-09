9463651a88e89dd14c839c4b1639423a4ac24e58
clamlum2 <clamlum2@gmail.com>
2025-10-09 20:14:32 +1300

    udpated laptop driver version

--- imports/laptop.nix
+++ imports/laptop.nix
@@ -4,7 +4,7 @@
   networking.hostName = "laptop";
 
   environment.systemPackages = with pkgs; [
-    pkgs.linuxKernel.packages.linux_6_16.broadcom_sta
+    pkgs.linuxKernel.packages.linux_6_12.broadcom_sta
     pkgs.upower
   ];
 
@@ -12,8 +12,7 @@
   boot.kernelModules = [ "wl" ];
   boot.blacklistedKernelModules = [ "b43" "ssb" "bcma" "brcm80211" "brcmfmac" "brcmsmac" "bcmdhd" ];
   nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-57-6.16.7"
-    "broadcom-sta-6.30.223.271-57-6.16.9"
+    "broadcom-sta-6.30.223.271-57-6.12.50"
   ];
 
   programs.nix-ld.enable = true;
