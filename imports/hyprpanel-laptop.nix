3df492756b7fe97e847c7f9c79d537763658ae4f
clamlum2 <clamlum2@gmail.com>
2025-10-13 00:09:31 +1300

    adjusted dashboard size

--- imports/hyprpanel-laptop.nix
+++ imports/hyprpanel-laptop.nix
@@ -531,6 +531,7 @@
             "menus.clock.weather.key": "003bd2c5c54b48d2a28154312250108",
             "theme.osd.enable": true,
             "bar.launcher.autoDetectIcon": true
+            "theme.dashboard.scaling": 85
         }
     '';
 }
