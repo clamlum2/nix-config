5827df3888cd0286a80d5ac6a794865de3414a7d
clamlum2 <clamlum2@gmail.com>
2025-10-13 00:18:17 +1300

    fixed syntax

--- imports/hyprpanel-laptop.nix
+++ imports/hyprpanel-laptop.nix
@@ -530,7 +530,7 @@
             "menus.clock.weather.unit": "metric",
             "menus.clock.weather.key": "003bd2c5c54b48d2a28154312250108",
             "theme.osd.enable": true,
-            "bar.launcher.autoDetectIcon": true
+            "bar.launcher.autoDetectIcon": true,
             "theme.dashboard.scaling": 85
         }
     '';
