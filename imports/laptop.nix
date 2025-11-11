77a295858f6ff199b683e14bedaf3a8fc51b6cd6
clamt <clamlum2@gmail.com>
2025-11-11 20:25:21 +1300

    driver exclusions

--- imports/laptop.nix
+++ imports/laptop.nix
@@ -12,7 +12,7 @@
   boot.kernelModules = [ "wl" ];
   boot.blacklistedKernelModules = [ "b43" "ssb" "bcma" "brcm80211" "brcmfmac" "brcmsmac" "bcmdhd" ];
   nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-57-6.12.57"
+    "broadcom-sta-6.30.223.271-59-6.12.57"
   ];
 
   programs.nix-ld.enable = true;
