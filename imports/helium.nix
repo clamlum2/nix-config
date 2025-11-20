af6b1c3bc8c5397f246ded1a13549ba57b26bcf0
clamlum2 <clamlum2@gmail.com>
2025-11-20 17:25:13 +1300

    updated helium

--- imports/helium.nix
+++ imports/helium.nix
@@ -2,10 +2,10 @@
 
 pkgs.stdenv.mkDerivation {
   pname = "helium-browser";
-  version = "0.6.5.1";
+  version = "0.6.7.1";
   src = pkgs.fetchurl {
-    url = "https://github.com/imputnet/helium-linux/releases/download/0.6.5.1/helium-0.6.5.1-x86_64.AppImage";
-    sha256 = "sha256-EHhIwNnLm7sTlbm5+OTWQl2FGnb1iaAD8S7P5RtIPu4=";
+    url = "https://github.com/imputnet/helium-linux/releases/download/0.6.7.1/helium-0.6.7.1-x86_64.AppImage";
+    sha256 = "sha256-fZTBNhaDk5EeYcxZDJ83tweMZqtEhd7ws8AFUcHjFLs=";
   };
   unpackPhase = "true";
   buildInputs = [ pkgs.appimage-run ];
