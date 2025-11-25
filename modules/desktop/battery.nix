82c2e8a6ab0ca2c8eb02852a2f742d16e47139e7
clamlum2 <clamlum2@gmail.com>
2025-11-26 04:03:43 +1300

    trying to make battery appear after systray

--- modules/desktop/battery.nix
+++ modules/desktop/battery.nix
@@ -1,5 +1,17 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
+
+let
+  existing = if config ? programs && config.programs ? hyprpanel && config.programs.hyprpanel ? settings && config.programs.hyprpanel.settings ? bar && config.programs.hyprpanel.settings.bar ? layouts && config.programs.hyprpanel.settings.bar.layouts
+    then config.programs.hyprpanel.settings.bar.layouts."*".right
+    else [];
+
+  hasSystray = builtins.any (x: x == "systray") existing;
+
+  inserted = if hasSystray
+    then builtins.concatLists (map (x: if x == "systray" then [ x "battery" ] else [ x ]) existing)
+    else builtins.concatLists [ existing [ "battery" ] ];
+in
 
 {
-  programs.hyprpanel.settings.bar.layouts."*".right = [ "battery" ];
+  programs.hyprpanel.settings.bar.layouts."*".right = lib.mkForce inserted;
 }
