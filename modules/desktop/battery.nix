060ac6f9f37a13b272a57df4fa40612d8543e44e
clamlum2 <clamlum2@gmail.com>
2025-11-26 04:09:25 +1300

    testing

--- modules/desktop/battery.nix
+++ modules/desktop/battery.nix
@@ -1,17 +1,4 @@
-{ config, lib, ... }:
-
-let
-  existing = if config ? programs && config.programs ? hyprpanel && config.programs.hyprpanel ? settings && config.programs.hyprpanel.settings ? bar && config.programs.hyprpanel.settings.bar ? layouts && config.programs.hyprpanel.settings.bar.layouts ? "*" && config.programs.hyprpanel.settings.bar.layouts."*" ? right
-    then config.programs.hyprpanel.settings.bar.layouts."*".right
-    else [];
-
-  hasSystray = builtins.any (x: x == "systray") existing;
-
-  inserted = if hasSystray
-    then builtins.concatLists (map (x: if x == "systray" then [ x "battery" ] else [ x ]) existing)
-    else builtins.concatLists [ existing [ "battery" ] ];
-in
+{ /* no-op battery helper; insertion handled in `hyprpanel.nix` */ }:
 
 {
-  programs.hyprpanel.settings.bar.layouts."*".right = lib.mkForce inserted;
 }
