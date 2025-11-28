9b47ee2d4e64639cd66e2cfef29a9fac5cd7a9d3
clamlum2 <clamlum2@gmail.com>
2025-11-29 11:28:00 +1300

    updated laptop to latest kernel

--- modules/devices/laptop.nix
+++ modules/devices/laptop.nix
@@ -8,13 +8,13 @@
     pkgs.upower
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
   boot.kernelModules = [ "wl" ];
   boot.blacklistedKernelModules = [ "b43" "ssb" "bcma" "brcm80211" "brcmfmac" "brcmsmac" "bcmdhd" ];
   nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-59-6.12.58"
+
   ];
 
   programs.nix-ld.enable = true;
