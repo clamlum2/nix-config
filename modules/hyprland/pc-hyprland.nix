c5d7f8b93acab7871f210871cb27220656aaf291
clamlum2 <clamlum2@gmail.com>
2026-01-11 15:58:58 +1300

    changed hyprland workspace rules

--- modules/hyprland/pc-hyprland.nix
+++ modules/hyprland/pc-hyprland.nix
@@ -8,10 +8,12 @@
     ];
 
     workspace = [
-      "1, monitor:DP-2, persistent:true, default:true"
-      "2, monitor:DP-1, persistent:true"
-      "7, monitor:DP-1, persistent:true"
-      "8, monitor:DP-1, persistent:true"
+      "1, monitor:DP-2, default:true"
+      "2, monitor:DP-1,"
+      "3, monitor:DP-2,"
+      "4, monitor:DP-2,"
+      "7, monitor:DP-1,"
+      "8, monitor:DP-1,"
     ];
   };
 }
