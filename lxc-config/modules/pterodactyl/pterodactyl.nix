d931411d6fe51df6f94735f7a15f746303dbe7e1
clamlum2 <clamlum2@gmail.com>
2026-01-12 10:26:53 +0000

    pterodactyl config

--- lxc-config/modules/pterodactyl/pterodactyl.nix
+++ lxc-config/modules/pterodactyl/pterodactyl.nix
@@ -1,44 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.nginx
-    pkgs.mariadb
-    pkgs.redis
-    pkgs.docker
-    pkgs.git
-    pkgs.unzip
-    pkgs.curl
-    pkgs.nodejs
-    pkgs.yarn
-    pkgs.php
-    pkgs.phpPackages.pdo_mysql
-    pkgs.phpPackages.mbstring
-    pkgs.phpPackages.zip
-    pkgs.phpPackages.gd
-    pkgs.phpPackages.curl
-  ];
+  imports = [ ./pterodactyl-service.nix ];
 
-  services.nginx.enable = true;
-  services.phpfpm.enable = true;
-  services.mariadb.enable = true;
-  services.redis.enable = true;
-  services.docker.enable = true;
-
-  users.users.pterodactyl = {
-    isNormalUser = true;
-    extraGroups = [ "nginx" "docker" ];
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
   };
 
-  systemd.services.pterodactyl-panel = {
-    description = "Pterodactyl Panel";
-    wants = [ "mysql.service" "php-fpm.service" "redis.service" ];
-    after = [ "mysql.service" "php-fpm.service" "redis.service" ];
-    serviceConfig.ExecStart = "${pkgs.php}/bin/php /srv/pterodactyl/artisan serve --host=0.0.0.0 --port=8080";
-    serviceConfig.User = "pterodactyl";
-    serviceConfig.WorkingDirectory = "/srv/pterodactyl";
-    restart = "always";
+  services.pterodactyl = {
+    enable = true;
+    nginxVhost = "localhost";
+    user = "pterodactyl";
+    dataDir = "/srv/pterodactyl";
+    redisName = "pterodactis";
   };
-
-  environment.etc."srv/pterodactyl".source = ./pterodactyl;
 }
