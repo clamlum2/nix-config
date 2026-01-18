4d503d86ed9043dd88515f446dc5d4cc30e982cf
clamlum2 <clamlum2@gmail.com>
2026-01-18 19:34:01 +1300

    added virtualisation config

---
+++ modules/pc/virtualisation.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.libvirtd.enable = true;
+
+  virtualisation.libvirtd.qemu = {
+    package = pkgs.qemu_kvm;
+    runAsRoot = true;
+  };
+
+  programs.virt-manager.enable = true;
+
+  networking.firewall.checkReversePath = false;
+
+  users.users.clamt = {
+    extraGroups = [ "libvirtd" "kvm" ];
+  };
+}
