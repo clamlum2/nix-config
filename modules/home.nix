07f2474c09c1bc67d094d02cad3885f44a2c7a9f
clamlum2 <clamlum2@gmail.com>
2026-01-18 21:29:46 +1300

    added nix.conf

--- modules/home.nix
+++ modules/home.nix
@@ -11,4 +11,8 @@ in
   home.username = "clamt";
   home.homeDirectory = "/home/clamt";
   home.stateVersion = "25.11";
+
+  home.file.".config/nix/nix.conf".text = ''
+    experimental-features = nix-command flakes
+  '';
 }
