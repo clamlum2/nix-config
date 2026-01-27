34212a2b188ad684ce2193b6f7256bca13f245fe
clamlum2 <clamlum2@gmail.com>
2026-01-27 19:32:16 +1300

    opened port 8080

--- lxc-config/modules/mediaserver/mediaserver.nix
+++ lxc-config/modules/mediaserver/mediaserver.nix
@@ -4,4 +4,9 @@
   environment.etc."docker/mediaserver/docker-compose.yml".source = ../../resources/mediaserver/mediaserver-compose.yml;
 
   networking.hostName = "mediaserver";
+
+  networking.firewall = {
+    allowedTCPPorts = [ 8080 ];
+    allowedUDPPorts = [ ];
+  };
 }
