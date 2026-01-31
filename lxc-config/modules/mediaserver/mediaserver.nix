6102adc13bf7b1cb5cd673891cefa2c7ec7f0270
clamlum2 <clamlum2@gmail.com>
2026-01-31 19:04:16 +1300

    added user to mediaserver

--- lxc-config/modules/mediaserver/mediaserver.nix
+++ lxc-config/modules/mediaserver/mediaserver.nix
@@ -9,4 +9,23 @@
     allowedTCPPorts = [ 5055 7878 8080 8989 9696 ];
     allowedUDPPorts = [ ];
   };
+
+  users.users.chris = {
+    isNormalUser = true;
+    description = "chris";
+    packages = with pkgs; [];
+  };
+
+  services.samba = {
+    enable = true;
+    settings = {
+      media = {
+        path = "/mnt/drive/media";
+        browseable = true;
+        "read only" = false;
+        "guest ok" = false;
+        "valid users" = " chris ";
+      };
+    };
+  };
 }
