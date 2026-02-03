95c30b14c8384aa45c3c222de02c61ed1f54dbbd
clamlum2 <clamlum2@gmail.com>
2026-02-03 19:29:19 +1300

    moved pc specific configuration

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -9,20 +9,7 @@ in
 {
   system.stateVersion = "25.11";
 
-  imports = [ ];
-
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-
   networking.networkmanager.enable = true;
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 22 ];
-    allowedUDPPorts = [ ];
-  };
 
   services.openssh.enable = true;
 
@@ -50,15 +37,7 @@ in
     shell = pkgs.zsh;
   };
 
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
-  };
-
   programs.nix-ld.enable = true;
 
   environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
-
-  services.hardware.openrgb.enable = true;
 }
--- modules/pc/pc.nix
+++ modules/pc/pc.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
 {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
   fileSystems."/mnt/Games" = {
     device = "/dev/disk/by-uuid/44B26CAFB26CA760";
     fsType = "ntfs-3g";
@@ -21,6 +25,7 @@
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-zen4;
 
   networking.firewall = {
+    enable = true;
     allowedTCPPorts = [ 22 25565 ];
     allowedUDPPorts = [ 25565 ];
   };
@@ -31,4 +36,12 @@
     capSysAdmin = true;
     openFirewall = true;
   };
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  services.hardware.openrgb.enable = true;
 }
