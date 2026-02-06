44e8cc351dc0fe49d020e7774ab638796b5285d1
clamlum2 <clamlum2@gmail.com>
2026-02-06 19:14:31 +1300

    added video group

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -30,7 +30,7 @@ in
 
   users.users.clamt = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "video "];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/sXIx+I7BCq6T4QfiEWqvh+E1d9+y4CrTijURf5Wsq clamt"
     ];
