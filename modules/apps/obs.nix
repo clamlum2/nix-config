add1f043302368f34493ad4fa8313345c15abd3f
clamlum2 <clamlum2@gmail.com>
2026-02-06 19:14:43 +1300

    added v4l2loopback

--- modules/apps/obs.nix
+++ modules/apps/obs.nix
@@ -14,4 +14,14 @@ in
       cudaSupport = true;
     };
   };
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+
+  boot.kernelModules = [ "v4l2loopback" ];
+
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=10 card_label="OBS Virtual Camera" exclusive_caps=1
+  '';
 }
