50448b80925063023446e8681cc3fb65b6dcdeec
clamlum2 <clamlum2@gmail.com>
2026-02-10 18:31:22 +1300

    added yazi config

---
+++ modules/apps/yazi.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.yazi
+  ];
+
+  programs.yazi = {
+    enable = true;
+    settings = {
+    };
+  };
+
+  home.file.".zshrc".text = ''
+    function y() {
+      local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+      command yazi "$@" --cwd-file="$tmp"
+      IFS= read -r -d $'\0' cwd < "$tmp"
+      [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+      rm -f -- "$tmp"
+    }
+  '';
+}
