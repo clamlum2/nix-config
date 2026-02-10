e5f1ed9854cbc51f18345c68ee2c201ea21f888b
clamlum2 <clamlum2@gmail.com>
2026-02-10 18:31:17 +1300

    added micro config

---
+++ modules/apps/micro.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.micro
+  ];
+
+  home.file.".config/micro/colorschemes/custom.micro".source = ../../resources/micro/blue.micro;
+	home.file.".config/micro/settings.json".source = ../../resources/micro/settings.json;
+}
