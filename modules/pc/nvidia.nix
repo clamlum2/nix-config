e06529270a6d43967d20742653e97a0da649afc0
clamlum2 <clamlum2@gmail.com>
2026-02-14 16:12:27 +1300

    added nvidia driver patch for latest kernel

--- modules/pc/nvidia.nix
+++ modules/pc/nvidia.nix
@@ -1,4 +1,6 @@
 { config, lib, pkgs, ... }:
+
+
 {
   hardware.graphics = {
     enable = true;
@@ -13,11 +15,27 @@
 
     powerManagement.finegrained = false;
 
-    open = true;
-
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    open = true;
+
+    package =
+    let
+      base = config.boot.kernelPackages.nvidiaPackages.latest;
+      cachyos-nvidia-patch = pkgs.fetchpatch {
+      url = "https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/nvidia/nvidia-utils/kernel-6.19.patch";
+        sha256 = "sha256-YuJjSUXE6jYSuZySYGnWSNG5sfVei7vvxDcHx3K+IN4=";
+      };
+      in
+      # Use the // operator to merge the original bundle with your patched open module
+      base
+      // {
+      open = base.open.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or [ ]) ++ [ cachyos-nvidia-patch ];
+      });
+    };
+
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   environment.systemPackages = [
