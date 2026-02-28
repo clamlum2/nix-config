f534270145abd8c7055ed5bcac1450c1a12058a9
clamlum2 <clamlum2@gmail.com>
2026-02-28 18:07:13 +1300

    removed nvenc support

--- modules/apps/obs.nix
+++ modules/apps/obs.nix
@@ -1,27 +1,13 @@
-{ config, pkgs, nixpkgs-stable, ... }:
-
-let
-  stable = import nixpkgs-stable {
-    system = pkgs.system or "x86_64-linux";
-    config.allowUnfree = true;
-  };
-in
+{ config, pkgs, ... }:
 
 {
   programs.obs-studio = {
     enable = true;
-    package = stable.obs-studio.override {
-      cudaSupport = true;
-    };
   };
 
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    v4l2loopback
+  environment.systemPackages = [
+    pkgs.libva-vdpau-driver
+    pkgs.libva-utils
+    pkgs.ffmpeg-full
   ];
-
-  boot.kernelModules = [ "v4l2loopback" ];
-
-  boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=10 card_label="OBS Virtual Camera" exclusive_caps=1
-  '';
 }
