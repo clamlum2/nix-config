9aba8a12f1f54f128822c6b574ce9d9a7227d201
clamlum2 <clamlum2@gmail.com>
2026-02-28 18:07:19 +1300

    added gtk config

---
+++ modules/desktop/gtk.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  programs.dconf.enable = true;
+
+  environment.systemPackages = [
+    pkgs.glib
+    pkgs.gsettings-desktop-schemas
+    pkgs.gnome-themes-extra
+    pkgs.adwaita-icon-theme
+  ];
+}
