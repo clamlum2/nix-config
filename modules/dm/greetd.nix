14e7b6dd8e26703fe8355d45ade511eeda5e4130
clamlum2 <clamlum2@gmail.com>
2026-03-06 13:34:23 +1300

    tweaked greetd command

--- modules/dm/greetd.nix
+++ modules/dm/greetd.nix
@@ -9,7 +9,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "tuigreet --cmd /run/current-system/sw/bin/niri-session -r --theme 'border=white;text=white;prompt=white;time=white;action=white;button=white;container=black;input=white'";
+        command = "tuigreet --cmd ${pkgs.niri}/bin/niri-session -r --theme 'border=white;text=white;prompt=white;time=white;action=white;button=white;container=black;input=white'";
         user = "greeter";
       };
     };
