14084f4522977c0a96615c6e31203bd470ff177e
clamlum2 <clamlum2@gmail.com>
2026-03-14 14:12:27 +1300

    added desktop file for helium

--- modules/apps/helium.nix
+++ modules/apps/helium.nix
@@ -1,13 +1,32 @@
-{ fetchurl, appimageTools, stdenv }:
+{ fetchurl, appimageTools, makeDesktopItem }:
 
 let
   pname = "helium-browser";
-  version = "0.10.2.1";
+  version = "0.10.4.1";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
     sha256 = "sha256-Kh6UgdleK+L+G4LNiQL2DkQIwS43cyzX+Jo6K0/fX1M=";
   };
+
+  desktopItem = makeDesktopItem {
+    name = pname;
+    desktopName = "Helium";
+    comment = "Web browser";
+    exec = "${pname} %U";
+    terminal = false;
+    categories = [ "Network" "WebBrowser" ];
+    icon = pname;
+    startupWMClass = "helium-browser";
+  };
 in
 
-appimageTools.wrapType2 { inherit pname version src; }
+appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -Dm444 ${desktopItem}/share/applications/${pname}.desktop \
+      -t "$out/share/applications"
+  '';
+}
+
