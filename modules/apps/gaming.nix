99569385f9788f4d655ce69bef75e925d0e2eabe
clamlum2 <clamlum2@gmail.com>
2026-04-29 02:56:07 +1200

    switched lutris to stable

--- modules/apps/gaming.nix
+++ modules/apps/gaming.nix
@@ -1,8 +1,15 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nixpkgs-stable, ... }:
+
+let
+  stable = import nixpkgs-stable {
+    system = pkgs.system or "x86_64-linux";
+    config.allowUnfree = true;
+  };
+in
 
 {
   environment.systemPackages = [
-    pkgs.lutris
+    stable.lutris
     pkgs.protonup-qt
     pkgs.prismlauncher
     pkgs.jdk25
