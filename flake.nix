a41efda1cf71ca34a28f7c7fde4518abf39fd936
clamlum2 <clamlum2@gmail.com>
2026-05-04 02:02:34 +1200

    cleaned up flake

--- flake.nix
+++ flake.nix
@@ -18,227 +18,240 @@
     nixos-lxc.url = "path:./lxc-config";
   };
 
-  outputs = inputs@ {
-    self,
-    nixpkgs,
-    nixpkgs-stable,
-    home-manager,
-    nix-cachyos-kernel,
-    lanzaboote,
-    nixos-wsl,
-    nixos-lxc,
-    ...
-  }:
-  {
-    nixosConfigurations = {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      home-manager,
+      nix-cachyos-kernel,
+      lanzaboote,
+      nixos-wsl,
+      nixos-lxc,
+      ...
+    }:
+    {
+      nixosConfigurations = {
 
-      ##################
-      # PC Configuration
-      ##################
+        ##################
+        # PC Configuration
+        ##################
 
-      nixos = let
-        system = "x86_64-linux";
-      in nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [z
-          ({ ... }: {
-            nixpkgs.overlays = [
-              nix-cachyos-kernel.overlays.pinned
+        nixos =
+          let
+            system = "x86_64-linux";
+          in
+          nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
+              (
+                { ... }:
+                {
+                  nixpkgs.overlays = [
+                    nix-cachyos-kernel.overlays.pinned
+                  ];
+                }
+              )
+              ./modules/devices/pc/hardware-configuration.nix
+
+              ./modules/devices/pc/pc.nix
+              ./modules/devices/pc/amd.nix
+              # ./modules/devices/pc/virtualisation.nix
+
+              ./modules/configuration.nix
+              ./modules/pkgs.nix
+
+              ./modules/desktops/niri/niri.nix
+
+              ./modules/desktops/dm/greetd.nix
+
+              ./modules/desktops/services/fonts.nix
+              ./modules/desktops/services/audio.nix
+              ./modules/desktops/services/gtk.nix
+
+              ./modules/apps/gaming.nix
+              ./modules/apps/obs.nix
+              ./modules/apps/vesktop.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  backupFileExtension = "backup";
+                  extraSpecialArgs = {
+                    nixpkgs-stable = nixpkgs-stable;
+                    self = self;
+                    repoRoot = ./.;
+                  };
+                  users.clamt =
+                    { ... }:
+                    {
+                      imports = [
+                        ./modules/home.nix
+
+                        ./modules/desktops/niri/niri-config.nix
+
+                        ./modules/desktops/services/icons.nix
+                        ./modules/desktops/services/mako.nix
+
+                        ./modules/shell/zsh.nix
+                        ./modules/shell/themes/blue.nix
+
+                        ./modules/apps/bars/quickshell.nix
+
+                        ./modules/apps/terminals/ghostty.nix
+                        ./modules/apps/terminals/wezterm.nix
+                        ./modules/apps/terminals/kitty.nix
+
+                        ./modules/apps/fuzzel.nix
+                        ./modules/apps/yazi.nix
+                        ./modules/apps/micro.nix
+                      ];
+                    };
+                };
+              }
+              lanzaboote.nixosModules.lanzaboote
+              (
+                { pkgs, lib, ... }:
+                {
+
+                  environment.systemPackages = [
+                    pkgs.sbctl
+                  ];
+
+                  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+                  boot.lanzaboote = {
+                    enable = true;
+                    pkiBundle = "/var/lib/sbctl";
+                  };
+                }
+              )
             ];
-          })
-          ./modules/devices/pc/hardware-configuration.nix
-
-          ./modules/devices/pc/pc.nix
-          ./modules/devices/pc/amd.nix
-          ./modules/devices/pc/virtualisation.nix
-
-          ./modules/configuration.nix
-          ./modules/pkgs.nix
-
-          ./modules/desktops/niri/niri.nix
-
-          ./modules/desktops/dm/greetd.nix
-
-          ./modules/desktops/services/fonts.nix
-          ./modules/desktops/services/audio.nix
-          ./modules/desktops/services/gtk.nix
-
-          ./modules/apps/gaming.nix
-          ./modules/apps/obs.nix
-          ./modules/apps/vesktop.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                nixpkgs-stable = nixpkgs-stable;
-                self = self;
-                repoRoot = ./.;
-              };
-              users.clamt = { pkgs, ... }: {
-                imports = [
-                  ./modules/home.nix
-
-                  ./modules/desktops/niri/niri-config.nix
-
-                  ./modules/desktops/services/icons.nix
-                  ./modules/desktops/services/mako.nix
-
-                  ./modules/shell/zsh.nix
-                  ./modules/shell/themes/blue.nix
-
-                  ./modules/apps/bars/quickshell.nix
-
-                  ./modules/apps/terminals/ghostty.nix
-                  ./modules/apps/terminals/wezterm.nix
-                  ./modules/apps/terminals/kitty.nix
-
-                  ./modules/apps/fuzzel.nix
-                  ./modules/apps/yazi.nix
-                  ./modules/apps/micro.nix
-                ];
-              };
+            specialArgs = {
+              inherit nixpkgs-stable;
+              inherit self;
             };
-          }
-          lanzaboote.nixosModules.lanzaboote
-          ({ pkgs, lib, ... }: {
+          };
 
-            environment.systemPackages = [
-              pkgs.sbctl
+        ######################
+        # Laptop Configuration
+        ######################
+
+        laptop =
+          let
+            system = "x86_64-linux";
+          in
+          nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
+              ./modules/devices/laptop/hardware-configuration.nix
+
+              ./modules/devices/laptop/laptop.nix
+
+              ./modules/configuration.nix
+              ./modules/pkgs.nix
+
+              ./modules/desktops/niri/niri.nix
+
+              ./modules/desktops/greetd.nix
+
+              ./modules/desktops/services/fonts.nix
+              ./modules/desktops/services/audio.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  backupFileExtension = "backup";
+                  extraSpecialArgs = {
+                    nixpkgs-stable = nixpkgs-stable;
+                    self = self;
+                    repoRoot = ./.;
+                  };
+                  users.clamt = {
+                    imports = [
+                      ./modules/home.nix
+
+                      ./modules/desktops/niri/niri-config.nix
+
+                      ./modules/desktops/services/icons.nix
+                      ./modules/desktops/services/mako.nix
+
+                      ./modules/shell/zsh.nix
+                      ./modules/shell/themes/blue.nix
+
+                      ./modules/apps/bars/quickshell.nix
+
+                      ./modules/apps/terminals/ghostty.nix
+                      ./modules/apps/terminals/wezterm.nix
+                      ./modules/apps/terminals/kitty.nix
+
+                      ./modules/apps/fuzzel.nix
+                    ];
+                  };
+                };
+              }
             ];
-
-            boot.loader.systemd-boot.enable = lib.mkForce false;
-
-            boot.lanzaboote = {
-              enable = true;
-              pkiBundle = "/var/lib/sbctl";
+            specialArgs = {
+              inherit nixpkgs-stable;
+              inherit self;
             };
-          })
-        ];
-        specialArgs = {
-          inherit nixpkgs-stable;
-          inherit self;
-        };
-      };
+          };
 
-      ######################
-      # Laptop Configuration
-      ######################
+        ###################
+        # WSL Configuration
+        ###################
 
-      laptop = let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-      in nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [
-          ./modules/devices/laptop/hardware-configuration.nix
+        wsl =
+          let
+            system = "aarch64-linux";
+          in
+          nixpkgs.lib.nixosSystem {
+            system = system;
+            modules = [
+              ./modules/devices/wsl/wsl.nix
 
-          ./modules/devices/laptop/laptop.nix
+              ./modules/desktops/services/fonts.nix
+              home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  backupFileExtension = "backup";
+                  extraSpecialArgs = {
+                    nixpkgs-stable = nixpkgs-stable;
+                    self = self;
+                    repoRoot = ./.;
+                  };
+                  users.clamt = {
+                    imports = [
+                      ./modules/devices/wsl/wsl-home.nix
 
-          ./modules/configuration.nix
-          ./modules/pkgs.nix
-
-          ./modules/desktops/niri/niri.nix
-
-          ./modules/desktops/greetd.nix
-
-          ./modules/desktops/services/fonts.nix
-          ./modules/desktops/services/audio.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                nixpkgs-stable = nixpkgs-stable;
-                self = self;
-                repoRoot = ./.;
-              };
-              users.clamt = { pkgs, ... }: {
-                imports = [
-                  ./modules/home.nix
-
-                  ./modules/desktops/niri/niri-config.nix
-
-                  ./modules/desktops/services/icons.nix
-                  ./modules/desktops/services/mako.nix
-
-                  ./modules/shell/zsh.nix
-                  ./modules/shell/themes/blue.nix
-
-                  ./modules/apps/bars/quickshell.nix
-
-                  ./modules/apps/terminals/ghostty.nix
-                  ./modules/apps/terminals/wezterm.nix
-                  ./modules/apps/terminals/kitty.nix
-
-                  ./modules/apps/fuzzel.nix
-                ];
-              };
+                      ./modules/shell/zsh.nix
+                      ./modules/shell/themes/blue.nix
+                    ];
+                  };
+                };
+              }
+              nixos-wsl.nixosModules.default
+              {
+                system.stateVersion = "25.11";
+                wsl.enable = true;
+              }
+            ];
+            specialArgs = {
+              inherit nixpkgs-stable;
+              inherit self;
             };
-          }
-        ];
-        specialArgs = {
-          inherit nixpkgs-stable;
-          inherit self;
-        };
+          };
+
+      }
+      // {
+        lxc = nixos-lxc.nixosConfigurations.lxc;
+        pterodactyl = nixos-lxc.nixosConfigurations.pterodactyl;
+        mediaserver = nixos-lxc.nixosConfigurations.mediaserver;
+        vaultwarden = nixos-lxc.nixosConfigurations.vaultwarden;
       };
-
-      ###################
-      # WSL Configuration
-      ###################
-
-      wsl = let
-        system = "aarch64-linux";
-        pkgs = import nixpkgs { inherit system; };
-      in nixpkgs.lib.nixosSystem {
-        system = system;
-        modules = [
-          ./modules/devices/wsl/wsl.nix
-
-          ./modules/desktops/services/fonts.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              extraSpecialArgs = {
-                nixpkgs-stable = nixpkgs-stable;
-                self = self;
-                  repoRoot = ./.;
-              };
-              users.clamt = { pkgs, ... }: {
-                imports = [
-                  ./modules/devices/wsl/wsl-home.nix
-
-                  ./modules/shell/zsh.nix
-                  ./modules/shell/themes/blue.nix
-                ];
-              };
-            };
-          }
-          nixos-wsl.nixosModules.default
-          {
-            system.stateVersion = "25.11";
-            wsl.enable = true;
-          }
-        ];
-        specialArgs = {
-          inherit nixpkgs-stable;
-          inherit self;
-        };
-      };
-
-
-    } // {
-      lxc = nixos-lxc.nixosConfigurations.lxc;
-      pterodactyl = nixos-lxc.nixosConfigurations.pterodactyl;
-      mediaserver = nixos-lxc.nixosConfigurations.mediaserver;
-      vaultwarden = nixos-lxc.nixosConfigurations.vaultwarden;
     };
-  };
 }
