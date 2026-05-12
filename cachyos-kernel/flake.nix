d60006fbb110c5e837d0b87f5a25775e40d6a4d1
clamlum2 <clamlum2@gmail.com>
2026-05-12 14:22:19 +1200

    created flake for custom cachyos kernel

---
+++ cachyos-kernel/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "personal cachyos kernel flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      cachyVersion = "7.1-rc3";
+      cachyRelease = "1";
+      modDirVersion = "7.1.0-rc3-cachyos";
+
+      version = "${cachyVersion}-${cachyRelease}";
+    in
+    {
+      packages.x86_64-linux.kernel = pkgs.linuxManualConfig {
+        inherit version modDirVersion;
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/CachyOS/linux/releases/download/cachyos-${version}/cachyos-${version}.tar.gz";
+          hash = "sha256-MYhV5ujJDHsBMNpKhNkQt+7YwbQxw7cu/tFtyarxbTM=";
+        };
+
+        configfile = ./config;
+        allowImportFromDerivation = true;
+      };
+    };
+}
