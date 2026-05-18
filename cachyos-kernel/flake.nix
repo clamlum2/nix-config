ab1aa85d5bade8256543f9caf58ba66d086299b0
clamlum2 <clamlum2@gmail.com>
2026-05-19 10:19:04 +1200

    kernel update

--- cachyos-kernel/flake.nix
+++ cachyos-kernel/flake.nix
@@ -5,9 +5,9 @@
     { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      cachyVersion = "7.0.8";
+      cachyVersion = "7.1-rc4";
       cachyRelease = "1";
-      modDirVersion = "7.0.8-cachyos";
+      modDirVersion = "7.1.0-rc4-cachyos";
       version = "${cachyVersion}-${cachyRelease}";
     in
     {
@@ -16,7 +16,7 @@
           inherit version modDirVersion;
           src = pkgs.fetchurl {
             url = "https://github.com/CachyOS/linux/releases/download/cachyos-${version}/cachyos-${version}.tar.gz";
-            hash = "sha256-TEwhSelYxxDoWW7GeJsr4t/LFMGR94VRLHdT99ia3HU=";
+            hash = "sha256-4bKV0R/O9I6Tq0YDyXRehuIC+NAUV32OPzm4/YPm/4g=";
           };
           configfile = ./kernel_config;
           allowImportFromDerivation = true;
@@ -25,6 +25,7 @@
             passthru = (old.passthru or { }) // {
               features = (old.passthru.features or { }) // {
                 ia32Emulation = true;
+                efiBootStub = true;
               };
             };
           });
