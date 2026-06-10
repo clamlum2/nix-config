ddf9ca5fa2d8f0754153fd2b75328d398738f8f0
clamlum2 <clamlum2@gmail.com>
2026-06-10 17:44:27 +1200

    formatting

--- modules/devices/laptop/laptop.nix
+++ modules/devices/laptop/laptop.nix
@@ -16,17 +16,25 @@
     pkgs.linuxKernel.packages.linux_6_12.broadcom_sta
 
     pkgs.brightnessctl
+    pkgs.btop
   ];
 
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
     extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
     kernelModules = [ "wl" ];
-    blacklistedKernelModules = [ "b43" "ssb" "bcma" "brcm80211" "brcmfmac" "brcmsmac" "bcmdhd" ];
+    blacklistedKernelModules = [
+      "b43"
+      "ssb"
+      "bcma"
+      "brcm80211"
+      "brcmfmac"
+      "brcmsmac"
+      "bcmdhd"
+    ];
   };
 
-  nixpkgs.config.allowInsecurePredicate = pkg:
-  builtins.match "broadcom-sta-.*" pkg.name != null;
+  nixpkgs.config.allowInsecurePredicate = pkg: builtins.match "broadcom-sta-.*" pkg.name != null;
 
   services.upower.enable = true;
 }
