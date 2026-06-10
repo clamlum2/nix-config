d848c34e5dd5f3af4ace681c1f321f065a0cc0bd
clamlum2 <clamlum2@gmail.com>
2026-06-10 17:44:08 +1200

    added swaylock config

---
+++ modules/desktops/niri/swaylock.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+
+{
+  services.logind.lidSwitch = "suspend";
+
+  home-manager.users.clamt = {
+    programs.swaylock = {
+      enable = true;
+      package = pkgs.swaylock-effects;
+      settings = {
+        image = "/home/clamt/Pictures/wallpapers/353544.jpg";
+        effect-blur = "5x1";
+
+        indicator-radius = 40;
+        indicator-thickness = 8;
+        indicator-idle-visible = false;
+      };
+    };
+
+    services.swayidle = {
+      enable = true;
+      events = {
+        lock         = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+        before-sleep = "${pkgs.swaylock-effects}/bin/swaylock -fF";
+      };
+    };
+  };
+}
