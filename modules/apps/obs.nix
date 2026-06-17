a0b0c96534214c70468fcf6a84567688abaf00d8
clamlum2 <clamlum2@gmail.com>
2026-06-18 11:24:20 +1200

    added obs plugins

--- modules/apps/obs.nix
+++ modules/apps/obs.nix
@@ -3,6 +3,13 @@
 {
   programs.obs-studio = {
     enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-gstreamer
+      obs-vkcapture
+    ];
   };
 
   environment.systemPackages = [
