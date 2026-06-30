fda40d791302fbd5fcebd5fb5a62498b754cca2d
clamlum2 <clamlum2@gmail.com>
2026-06-30 12:57:09 +1200

    Update helium.nix

--- modules/apps/helium.nix
+++ modules/apps/helium.nix
@@ -13,7 +13,7 @@ let
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    sha256 = "sha256-z23up+T6bj6F+cQslmI92bEksIAw1OQHRIrmQSaaxY8=";
+    sha256 = "sha256-ZcZo/vFXWrZjuPjIt2MYbsxs4LU7NvpB3I6mrPzAJjE=";
   };
 
   extracted = appimageTools.extract {
