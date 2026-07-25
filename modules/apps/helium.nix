1174fd690866107495c5b9b0982e9f33ea301662
clamlum2 <clamlum2@gmail.com>
2026-07-25 17:02:24 +1200

    Update helium.nix

--- modules/apps/helium.nix
+++ modules/apps/helium.nix
@@ -7,13 +7,13 @@
 
 let
   pname = "helium-browser";
-  version = "0.14.6.1";
+  version = "0.14.8.2";
 
   extraFlags = "--password-store=basic";
 
   src = fetchurl {
     url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
-    sha256 = "sha256-qdM1Qysx5OOBwzr6A6tyPIfZcHxn2YkIPedGelvbk7I=";
+    sha256 = "sha256-7Kde6Crsr6LdKnfER+6yE7JkU6+10xXtJ1GQEHhBtqg=";
   };
 
   extracted = appimageTools.extract {
