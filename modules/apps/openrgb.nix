d2201ab39ee0607e814d2b8b95f4be1598aa85c2
clamlum <clamlum2@gmail.com>
2026-08-13 20:41:59 +1200

    update from plugin to openrgb patch

--- modules/apps/openrgb.nix
+++ modules/apps/openrgb.nix
@@ -1,57 +1,24 @@
 { pkgs, lib, username, ... }:
-
 let
-  openrgbPlugin = pkgs.stdenv.mkDerivation {
-    pname = "openrgb-asrock-rx9070xt-steel-legend-plugin";
-    version = "unstable";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "VirulentArc";
-      repo = "openrgb-asrock-rx9070xt-steel-legend-plugin";
-      rev = "main";
-      hash = "sha256-SPqjjuCgbBmQLaJx4hbYXV0hzbtseUyYGLiI2mHbwfE=";
-    };
-
-    nativeBuildInputs = with pkgs; [
-      qt6.qmake
-      qt6.qttools
-    ];
-
-    buildInputs = [ pkgs.qt6.qtbase ];
-
-    dontWrapQtApps = true;
-
-    buildPhase = ''
-      runHook preBuild
-
-      cp -r . "$TMPDIR/plugin"
-      chmod -R u+w "$TMPDIR/plugin"
-      cd "$TMPDIR/plugin"
-
-      substituteInPlace ASRockRX9070XTPlugin.cpp \
-        --replace-fail 'bus->bus_id != 7' 'bus->bus_id != 8'
-
-      OPENRGB_ROOT="${pkgs.openrgb.src}" \
-        qmake6 OpenRGBASRockRX9070XTPlugin.pro
-
-      make -j"$NIX_BUILD_CORES"
-
-      runHook postBuild
-    '';
-
-    installPhase = ''
-      mkdir -p $out/lib
-      cp "$TMPDIR/plugin/build/libOpenRGBASRockRX9070XTPlugin.so" \
-        $out/lib/
-    '';
+  asrockControllerSrc = pkgs.fetchFromGitHub {
+    owner = "VirulentArc";
+    repo = "openrgb-asrock-rx9070xt-steel-legend-controller";
+    rev = "main";
+    hash = "sha256-RHO3Cux6oknXNjPZipR66kLdy3vP/qH7Ao0ANEembTo=";
   };
-in
 
+  openrgbWithAsrock = pkgs.openrgb.overrideAttrs (old: {
+    postPatch = (old.postPatch or "") + ''
+      cp -r ${asrockControllerSrc}/Controllers/ASRockRX9070XTGPUController Controllers/
+      chmod -R u+w Controllers/ASRockRX9070XTGPUController
+    '';
+  });
+in
 {
   services.hardware.openrgb.enable = true;
   systemd.services.openrgb.enable = lib.mkForce false;
 
-  environment.systemPackages = [ pkgs.openrgb ];
+  environment.systemPackages = [ openrgbWithAsrock ];
 
   boot.kernelModules = [ "i2c-dev" ];
   hardware.i2c.enable = true;
@@ -63,12 +30,4 @@ in
   users.groups.i2c = {};
   users.users.${username}.extraGroups = [ "i2c" ];
 
-  home-manager.users.${username}.home.file.".config/OpenRGB/plugins/libOpenRGBASRockRX9070XTPlugin.so".source =
-    "${openrgbPlugin}/lib/libOpenRGBASRockRX9070XTPlugin.so";
-
-  system.activationScripts.openrgbRootPlugin = ''
-    mkdir -p /root/.config/OpenRGB/plugins
-    ln -sf ${openrgbPlugin}/lib/libOpenRGBASRockRX9070XTPlugin.so \
-      /root/.config/OpenRGB/plugins/libOpenRGBASRockRX9070XTPlugin.so
-  '';
 }
