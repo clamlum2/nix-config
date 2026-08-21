c63264759d8279b34a7d6ca9d251ad8e8d3100f3
clamlum <clamlum2@gmail.com>
2026-08-21 14:14:49 +1200

    add global ssh config

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -11,6 +11,32 @@
   security.pki.certificateFiles = [ ../resources/certs/ca.crt ];
 
   services.openssh.enable = true;
+  programs.ssh.extraConfig = ''
+    Host pc
+      HostName 192.168.2.1
+
+    Host laptop
+      HostName 192.168.2.2
+
+    Host media
+      HostName 192.168.2.10
+      User root
+  '';
+  programs.ssh.knownHosts = {
+    pc = {
+      hostNames = [ "pc" "192.168.2.1" ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDFpRxt/w44Zun2tL6DjOyVI7eVV4ISDHEy0dbMCbTGC";
+    };
+    laptop = {
+      hostNames = [ "laptop" "192.168.2.2" ];
+      publicKey = "192.168.2.2 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3LuO3HPPmYPk9nvgyfXf+rgg7AJCNJSQBcHzpzHA2w";
+    };
+    media = {
+      hostNames = [ "media" "192.168.2.10" ];
+      publicKey = "192.168.2.10 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPZiyCRx8WjX1Fmq63abOBVRYWC6ObrgYh0HxxtPUuM7";
+    };
+  };
+
 
   time.timeZone = "Pacific/Auckland";
   i18n.defaultLocale = "en_US.UTF-8";
