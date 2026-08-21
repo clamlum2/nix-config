02439144d3ce62ef653ada8066b4f35b83b9139c
clamlum <clamlum2@gmail.com>
2026-08-21 14:14:19 +1200

    switch back to simple linux config

--- modules/apps/kopuz.nix
+++ modules/apps/kopuz.nix
@@ -1,9 +1,5 @@
-{ pkgs, inputs, device, username, ... }:
+{ pkgs, inputs, username, ... }:
 let
-  configDir = if device == "macbook" then
-    "Library/Application Support/com.temidaradev.kopuz"
-  else ".config/kopuz";
-
   configFile = ''
     discord_presence = false
     settings_layout = "TopBar"
@@ -11,49 +7,10 @@ let
     ui_style = "Vaxry"
   '';
 in
-if device == "nixos" then
 {
   environment.systemPackages = [
-    inputs.custom-kopuz.packages.${pkgs.stdenv.hostPlatform.system}.default
+    inputs.kopuz.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
-  home-manager.users.${username}.home.file."${configDir}/settings.toml".text = configFile;
-}
-else let
-  system = pkgs.stdenv.hostPlatform.system;
-  kopuzSrc = inputs.kopuz;
-  kopuzBin = inputs.kopuz.packages.${system}.default;
-
-  kopuzApp = pkgs.stdenvNoCC.mkDerivation {
-    pname = "kopuz-app-bundle";
-    version = "unstable";
-    dontUnpack = true;
-
-    installPhase = ''
-      appdir="$out/Applications/Kopuz.app/Contents"
-      mkdir -p "$appdir/MacOS" "$appdir/Resources"
-
-      cp ${kopuzBin}/bin/kopuz "$appdir/MacOS/kopuz"
-      chmod +x "$appdir/MacOS/kopuz"
-
-      chmod +w "$appdir"
-
-      cp ${kopuzSrc}/crates/kopuz/assets/icon.icns "$appdir/Resources/Kopuz.icns"
-      cp ${kopuzSrc}/crates/kopuz/Info.plist "$appdir/Info.plist"
-    '';
-  };
-in
-{
-  nix.settings = {
-    substituters = [ "https://kopuz.cachix.org" ];
-    trusted-public-keys = [
-      "kopuz.cachix.org-1:J2X3AnAYhKTJW5S3aCLoA1ckonQXVNZMQvhZA0YAufw="
-    ];
-  };
-
-  environment.systemPackages = [
-    kopuzApp
-  ];
-
-  home-manager.users.${username}.home.file."${configDir}/settings.toml".text = configFile;
+  home-manager.users.${username}.home.file.".config/kopuz/settings.toml".text = configFile;
 }
